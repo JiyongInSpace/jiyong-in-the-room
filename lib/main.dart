a287da75fe951e2a1754e516eebed3ebfc427835
Jiyong Kim <gd693020@gmail.com>
2025-08-06 19:00:55 +0900

    [ADD] pretendard

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'Pretendard',
       ),
       home: HomeScreen(
         diaryList: diaryList,
