7de3b0b4c056aebd3b825dfce210790bce2c113c
Jiyong Kim <gd693020@gmail.com>
2025-08-08 23:28:57 +0900

    [UPDATE] android:label, namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.jiyong_in_the_room"
+    namespace = "com.jiyong.escape_diary"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.jiyong_in_the_room"
+        applicationId = "com.jiyong.escape_diary"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
